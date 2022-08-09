773092e915cd11526bb73526e6015b4d5852e830
Blake Mizerany <blake.mizerany@gmail.com>
2022-08-09 12:07:23 -0700

    Revert "cmd/tier: use stripe-cli login"
    This reverts commit 109d64b916f5de139d9914f0bb41a0791dd60539.

--- cmd/tier/connect.go
+++ cmd/tier/connect.go
@@ -1,26 +1,72 @@
 package main
 
 import (
-	"context"
-	"os"
+	"errors"
+	"fmt"
+	"strings"
 
-	"github.com/stripe/stripe-cli/pkg/config"
-	"github.com/stripe/stripe-cli/pkg/login"
+	"golang.org/x/term"
+
+	"github.com/zalando/go-keyring"
 )
 
 func connect() error {
-	cfg := &config.Config{}
-	cfg.LogLevel = "error"
-	cfg.InitConfig()
-	if err := login.Login(context.Background(), "https://dashboard.stripe.com", cfg, os.Stdin); err != nil {
+	fmt.Print(`To authenticate with Stripe, copy your API key from:
+
+	https://dashboard.stripe.com/test/apikeys (test key)
+	https://dashboard.stripe.com/apikeys      (live key)
+
+Stripe API key: `)
+
+	defer fmt.Println()
+
+	key, err := term.ReadPassword(0)
+	if err != nil {
 		return err
 	}
+
+	fmt.Print(strings.Repeat("*", 40))
+
+	if err := setKey(key); err != nil {
+		return err
+	}
+
+	fmt.Print(`
+
+Success.
+
+---------
+  -----
+    -
+
+Welcome to Tier.
+
+You're now ready to push a pricing model to Stripe using Tier. To learn more,
+please visit:
+
+	https://tier.run/docs/push
+`)
+
 	return nil
 }
 
+const keyringService = "tier.stripe.key"
+
+func setKey(data []byte) error {
+	key := string(data)
+	if !strings.HasPrefix(string(key), "sk_") {
+		return errors.New("invalid key: key must start with (\"sk_\") prefix")
+	}
+	if strings.HasPrefix(key, "sk_live_") {
+		return keyring.Set(keyringService, "live", key)
+	}
+	return keyring.Set(keyringService, "test", key)
+}
+
 func getKey(live bool) (string, error) {
-	cfg := &config.Config{}
-	cfg.LogLevel = "error"
-	cfg.InitConfig()
-	return cfg.Profile.GetAPIKey(live)
+	if live {
+		return keyring.Get(keyringService, "live")
+	} else {
+		return keyring.Get(keyringService, "test")
+	}
 }
