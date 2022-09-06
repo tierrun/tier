295938c1ab17f8563bf5819e7e2721fef11db1f9
Blake Mizerany <blake.mizerany@gmail.com>
2022-09-06 10:55:48 -0700

    add go generate to run.sh

--- docs/run.sh
+++ docs/run.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-# TODO: fix the need to navigate to /public in pages.
 echo "http://localhost:6060/"
-go generate
 go build -buildmode=plugin -o funcs.so .
 go run blake.io/pages/cmd/pages -http=:6060 -p funcs.so -v
