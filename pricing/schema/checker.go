52048011cb92f20bd545bc24b52f8f9a38a073b6
Blake Mizerany <blake.mizerany@gmail.com>
2022-09-08 14:50:05 -0700

    pricing: start validation

--- pricing/schema/checker.go
+++ pricing/schema/checker.go
@@ -2,35 +2,92 @@ package schema
 
 import (
 	"fmt"
-	"regexp"
 	"strings"
+	"unicode"
 
+	"golang.org/x/exp/slices"
 	"tailscale.com/util/multierr"
 )
 
-type Checker struct {
-	errs []error
+func Validate(p *Plan) error {
+	var e errors
+	e.report(validatePlanID(p.ID))
+	for _, f := range p.Features {
+		e.report(validateFeatureID(p.ID, f.ID))
+
+		if f.Base > 0 && len(f.Tiers) > 0 {
+			e.reportf("plans[%q].features[%q]: base must be zero with tiers", p.ID, f.ID)
+		}
+		if f.Base < 0 {
+			e.reportf("plans[%q].features[%q]: base must be positive", p.ID, f.ID)
+		}
+
+		for i, t := range f.Tiers {
+			if t.Upto < 0 {
+				e.reportf("plans[%q].features[%q].tiers[%d]: upto must be positive", p.ID, f.ID, i)
+			}
+			if t.Price < 0 {
+				e.reportf("plans[%q].features[%q].tiers[%d]: price must be positive", p.ID, f.ID, i)
+			}
+			if t.Base < 0 {
+				e.reportf("plans[%q].features[%q].tiers[%d]: base must be positive", p.ID, f.ID, i)
+			}
+		}
+	}
+	return multierr.New(e...)
 }
 
-var (
-	planRegexp = regexp.MustCompile(`^plan:[a-z0-9:]+@[a-z0-9]+$`)
-)
+func validatePlanID(id string) error {
+	var e errors
+	prefix, id, _ := strings.Cut(id, ":")
+	if prefix != "plan" {
+		e.reportf("plans[%q]: plan names must start with 'plan:'", id)
+	}
+	name, version, _ := strings.Cut(id, "@")
+	if !isAlphaNumeric(name, ":") {
+		e.reportf("plans[%q]: plan names must contain only letters, digits, or ':'s", id)
+	}
+	if version == "" {
+		e.reportf("plans[%q]: plan names must have a version", id)
+	}
+	if !isAlphaNumeric(version, "") {
+		e.reportf("plans[%q]: plan versions must contain only letters or digits", id)
+	}
+	return multierr.New(e...)
+}
 
-func (c *Checker) Plan(id string) {
-	if !planRegexp.MatchString(id) {
-		c.errs = append(c.errs, fmt.Errorf("plan ID (%q) must match %q", id, planRegexp.String()))
+func validateFeatureID(planID, id string) error {
+	var e errors
+	prefix, name, _ := strings.Cut(id, ":")
+	if prefix != "plan" {
+		e.reportf("plans[%q].features[%q]: plan names must start with 'feature:'", planID, id)
+	}
+	if !isAlphaNumeric(name, ":") {
+		e.reportf("plans[%q].features[%q]: feature names must contain only letters, digits, or ':'s", planID, id)
+	}
+	return multierr.New(e...)
+}
+
+type errors []error
+
+func (e *errors) report(err error) {
+	if err != nil {
+		*e = append(*e, err)
 	}
 }
 
-func (c *Checker) Feature(id string) {
-	if !strings.HasPrefix(id, "feature:") {
-		c.errs = append(c.errs, fmt.Errorf("feature ID (%q) must start with 'feature:'", id))
-	}
+func (e *errors) reportf(format string, args ...any) {
+	e.report(fmt.Errorf(format, args...))
 }
 
-func (c *Checker) Err() error {
-	if len(c.errs) == 0 {
-		return nil // maintain untyped nil
+func isAlphaNumeric(s string, extra string) bool {
+	for _, r := range s {
+		if slices.Contains([]rune(extra), r) {
+			continue
+		}
+		if !(unicode.IsDigit(r) || unicode.IsLetter(r)) {
+			return false
+		}
 	}
-	return multierr.New(c.errs...)
+	return true
 }
