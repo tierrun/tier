4c3e9c76a2e539f46ac6cfce72936782632b8d90
Blake Mizerany <blake.mizerany@gmail.com>
2022-10-21 17:48:57 -0700

    release.sh: fix build versioning (#108)

--- release.sh
+++ release.sh
@@ -1,3 +1,34 @@
 #!/bin/sh
 set -eu
+
+IFS=".$IFS" read -r major minor patch <VERSION.txt
+git_hash=$(git rev-parse HEAD)
+if ! git diff-index --quiet HEAD; then
+	git_hash="${git_hash}-dirty"
+fi
+base_hash=$(git rev-list --max-count=1 HEAD -- VERSION.txt)
+change_count=$(git rev-list --count HEAD "^$base_hash")
+short_hash=$(echo "$git_hash" | cut -c1-9)
+
+long_suffix="-t$short_hash"
+MINOR="$major.$minor"
+SHORT="$MINOR.$patch"
+LONG="${SHORT}$long_suffix"
+GIT_HASH="$git_hash"
+
+if [ "$1" = "shellvars" ]; then
+	cat <<EOF
+VERSION_MINOR="$MINOR"
+VERSION_SHORT="$SHORT"
+VERSION_LONG="$LONG"
+VERSION_GIT_HASH="$GIT_HASH"
+EOF
+	exit 0
+fi
+
+export VERSION_MINOR="$MINOR"
+export VERSION_SHORT="$SHORT"
+export VERSION_LONG="$LONG"
+export VERSION_GIT_HASH="$GIT_HASH"
+
 goreleaser release --rm-dist "$@"
