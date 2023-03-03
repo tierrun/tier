bf6fc2eb2d6c2cd215a9774302bf126258432058
Blake Mizerany <blake.mizerany@gmail.com>
2023-03-03 13:40:15 -0800

    client/tier: change Report to default to Stripe's now (#275)

--- client/tier/client_test.go
+++ client/tier/client_test.go
@@ -2,13 +2,17 @@ package tier
 
 import (
 	"context"
+	"encoding/json"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"sync"
 	"testing"
+	"time"
 
 	"kr.dev/diff"
+	"tier.run/api/apitypes"
+	"tier.run/refs"
 )
 
 func TestUserPassword(t *testing.T) {
@@ -35,5 +39,38 @@ func TestUserPassword(t *testing.T) {
 	}
 
 	want := []string{"testkey"}
+	diff.Test(t, t.Errorf, got, want)
+}
+
+func TestReportNow(t *testing.T) {
+	var mu sync.Mutex
+	var got []apitypes.ReportRequest
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		mu.Lock()
+		defer mu.Unlock()
+		var v apitypes.ReportRequest
+		if err := json.NewDecoder(r.Body).Decode(&v); err != nil {
+			t.Error(err)
+		}
+		got = append(got, v)
+		io.WriteString(w, "{}")
+	}))
+
+	c := &Client{BaseURL: s.URL}
+	if err := c.Report(context.Background(), "org:foo", "feature:x", 1); err != nil {
+		t.Fatal(err)
+	}
+
+	want := []apitypes.ReportRequest{
+		{
+			Org:     "org:foo",
+			Feature: refs.MustParseName("feature:x"),
+			N:       1,
+
+			// Check that At is unset causing use to use Stripe's 'now'.
+			At: time.Time{},
+		},
+	}
+
 	diff.Test(t, t.Errorf, got, want)
 }
